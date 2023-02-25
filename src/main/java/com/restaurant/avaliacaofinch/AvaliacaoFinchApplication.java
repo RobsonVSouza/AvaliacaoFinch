247b01768090fa85ffa23916172e81a285d2ebdf
Robson Souza <robson.ppmg@gmail.com>
2023-02-25 13:18:59 -0300

    creating project

---
+++ src/main/java/com/restaurant/avaliacaofinch/AvaliacaoFinchApplication.java
@@ -0,0 +1,13 @@
+package com.restaurant.avaliacaofinch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoFinchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoFinchApplication.class, args);
+	}
+
+}
